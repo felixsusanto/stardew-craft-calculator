ec9e6e86430d1daab42be156e550a65fd466be8c
Felix Susanto <felix.susanto2000@gmail.com>
2022-12-10 12:02:31 +0800

    base function done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import _ from 'lodash';
+import styled from 'styled-components';
+import craftableCsv from './csv/craftables.csv';
+import materialCsv from './csv/material.csv'; 
+// import * as Papa from 'papaparse';
+
+const zeroMask = (x: number) => {
+  if (x < 10) {
+    return `00${x}`;
+  } else if (x < 100) {
+    return `0${x}`;
+  }
+  return x + '';
+}
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+interface CraftableBase {
+  id: number;
+  name: string;
+}
+
+type Material = Record<string, number>;
+type Craftable = CraftableBase & Material;
+type CraftableProps = CraftableBase & {
+  material: Material;
+  onQtyChange: (v: Material) => void;
+};
+
+const CraftableComponent: React.FC<CraftableProps> = (props) => {
+  
+  const [qty, setQty] = useState(0);
+  
+  const [materialNeeded, setMaterialNeeded] = useState<Material>();
+  React.useEffect(() => {
+    const clone = {...props.material};
+    Object.keys(clone)
+      .forEach(key => {
+        const num = clone[key] * qty;
+        clone[key] = num;
+      })
+    ;
+    const filtered = _.omitBy(clone, (v) => v === 0);
+    props.onQtyChange(clone);
+    setMaterialNeeded(filtered);
+  }, [qty])
+  return (
+    <div>
+      <img src={`/img/${zeroMask(props.id)}.png`} />
+      { props.name }{' '}
+      <input type="number" value={qty} onChange={e => setQty(+e.currentTarget.value)}/>
+      <br />
+      {JSON.stringify(materialNeeded)}
+    </div>
+  );
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref = React.useRef(new Map<string, Material>());
+  const [craftable, setCraftable] = useState<Craftable[]>();
+  const [total, setTotal] = useState<Material>();
+  React.useEffect(() => {
+    const craftable: Craftable[] = craftableCsv;
+    setCraftable(craftable);
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Container>
+
+      <div className="App">
+        <div>{JSON.stringify(_.omitBy(total, (v) => v === 0))}</div>
+        <div className="card">
+          { craftable?.map((craft) => {
+            const {id, name, ...rest} = craft;
+            return (
+              <CraftableComponent 
+                key={id}
+                name={name}
+                id={id}
+                material={rest}
+                onQtyChange={(m) => {
+                  ref.current.set(name, m);
+                  const total = [...ref.current.values()]
+                    .reduce((acc, curr) => {
+                      Object.keys(curr)
+                        .forEach((key) => {
+                          const accVal = acc[key];
+                          const currVal = curr[key];
+                          if (!accVal) {
+                            acc[key] = currVal;
+                          } else {
+                            acc[key] = accVal + currVal;
+                          }
+                        })
+                      ;
+                      return acc;
+                    }, {})
+                  ;
+                  setTotal(total);
+                }}
+              />
+            );
+          }) }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Container>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dsv from '@rollup/plugin-dsv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    dsv({
+      processRow: (row, id) => {
+        Object.keys(row).forEach((key) => {
+          const value = row[key];
+          let result: any;
+          if (['FALSE', 'TRUE'].some(b => b === value)) {
+            result = value === 'TRUE';
+          } else {
+            result = (isNaN(+value) ? value : +value);
+          }
+          row[key] = result;
+        });
+        return undefined;
+      }
+    })
+  ],
 })
